4eaa6dfa97c8c1fbc1ada674f05454c177469fa1
Noam Rubin <rubin.m.noam@gmail.com>
2015-11-23 19:43:42 -0800

    added gmusic api

--- gmusic_api.py
+++
@@ -1,8 +0,0 @@
-# songbabel.gmusic@gmail.com
-# !Q@W3e4r
-from ghost import Ghost
-
-ghost = Ghost()
-
-page, extra_resources = ghost.open("http://jeanphi.fr")
-assert page.http_Status==200 and 'jeanphix' in ghost.content
